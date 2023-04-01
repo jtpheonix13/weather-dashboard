8bc0ee4303da31d2c49ab3e3c4b8c2ab81f1d6a6
jtpheonix13 <jgtihunt@comcast.net>
2023-04-01 18:43:13 -0500

    started script.js with a to do list

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// to do list
+
+// declare variables to pull from the html
+
+// set global variables
+
+// set a default city to populate the page with at the start
+
+// API Call
+
+// event handlers for the buttons
+
+// page initializing function
+
+// Go through api data for current forecast
+
+// go through api data for 5 day forecast
+
+// get weather data based on search input
+
+// get weather data based on past search
+
+// save past search and create button to display under search bar
+
+// save past search to local storage
+
+// load past searches from local storage
+
+// update weather cards with current data
